668bcb57d61b916d470bb4e19f2341b66ffaba88
Mykel Valadez <valadezmykel@gmail.com>
2020-11-16 17:44:44 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+const app = express();
+const PORT = process.env.PORT || 4400;
+
+app.use("/public", express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function(request, response){
+    response.sendFile(path.join(__dirname, "./html/index.html"));
+});
+
+app.post("*", function(req, res){
+    console.log("received");
+    res.send(true);
+})
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT"+ PORT);
+});
